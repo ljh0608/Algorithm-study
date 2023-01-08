8caca1738724539e441c5cd669b209abb1282686
ljh0608 <m98m@naver.com>
2023-01-08 21:29:24 +0900

    나머지연산_4375

---
+++ 4375_1.cpp
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+//4375
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+using namespace std;
+
+int N;
+int main() {
+
+	while (scanf("%d",&N)!=EOF) {
+	
+		int cnt = 1;
+		int ret = 1;
+		while (1) {
+			if (cnt%N == 0) {
+				cout << ret<<"\n";
+				break;
+			}
+			cnt = cnt * 10 + 1;
+			cnt %= N;
+			ret++;
+
+		
+		}	
+	}
+
+	}
+
+
+
+
