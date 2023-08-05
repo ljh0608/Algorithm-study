774c6dc06104034aa7c290a0f81c81b7bcdf94f4
ljh0608 <m98m@catholic.ac.kr>
2023-08-06 02:36:49 +0900

    2798_블랙잭

---
+++ 2798_블랙잭.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+int N, M;
+int result = 0;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	vector<int>v;
+	cin >> N >> M;
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		v.push_back(a);
+	}
+
+
+	for (int i = 0; i < N; i++) {
+		for (int j = i + 1; j < N; j++) {
+			for (int k = j + 1; k < N; k++) {
+				int temp = v[i] + v[j] + v[k];
+				if (temp <= M) {
+					result = max(result, temp);
+				}
+			}
+		}
+	}
+
+	cout << result;
+	return 0;
+}
