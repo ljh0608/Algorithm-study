9fb9bf07c3cc9db0ccc8fd812b5e4493e9eafd1f
ljh0608 <m98m@catholic.ac.kr>
2023-08-07 19:41:28 +0900

    9086_문자열

---
+++ 9086_문자열.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+int T;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		string s;
+		cin >> s;
+		cout << s[0] << s[s.length() - 1]<<"\n";
+		
+	}
+
+
+	return 0;
+}
