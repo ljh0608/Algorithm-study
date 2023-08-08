11c89bfc81b7ffd4c4bf10d496feeaf5c2bf5839
ljh0608 <m98m@catholic.ac.kr>
2023-08-08 10:58:31 +0900

    2908_상수

---
+++ 2908_상수.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	string s1, s2;
+
+	cin >> s1 >> s2;
+
+	reverse(s1.begin(), s1.end());
+	reverse(s2.begin(), s2.end());
+
+	if (stoi(s1) > stoi(s2)) {
+		cout << s1;
+	}
+	else {
+		cout << s2;
+	}
+
+
+	return 0;
+}
+
