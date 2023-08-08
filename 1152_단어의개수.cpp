3ee3271caf0f646021f344ea288e8e387e1dd122
ljh0608 <m98m@catholic.ac.kr>
2023-08-08 10:58:01 +0900

    1152_단어의개수

---
+++ 1152_단어의개수.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string s;
+	getline(cin, s);
+	int cnt = 0;
+
+	for (int i = 0; i < s.length(); i++) {
+		if (i!=0 &&i!=s.length()-1&&s[i] ==' ') {
+			cnt++;
+		}
+	}
+	if (s.length() == 1 && s[0] == ' ')
+	{
+		cout << 0;
+		return 0;
+	}
+	cout << cnt+1;
+
+	return 0;
+}
