960e17160821284460074e0cf420c6255aab27d5
ljh0608 <m98m@catholic.ac.kr>
2023-08-09 22:36:28 +0900

    11718_그대로출력하기

---
+++ 11718_그대로출력하기.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+int num;
+int result = 0;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	while (1) {
+		string str;
+		getline(cin, str);
+		if (str == "") {
+			break;
+		}
+		cout << str<<"\n";
+	}
+
+
+
+
+	return 0;
+}
+
