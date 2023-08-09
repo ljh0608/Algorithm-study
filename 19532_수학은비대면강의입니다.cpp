57c473da87e3c4ac7b660a732e74d63a688b8b50
ljh0608 <m98m@catholic.ac.kr>
2023-08-09 22:37:49 +0900

    19532_수학은비대면강의입니다

---
+++ 19532_수학은비대면강의입니다.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+int x, y;
+int a, b, c, d, e, f;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> a >> b >> c >> d >> e >> f;
+	x = (c * e - b * f) / (a * e - b * d);
+	y = (d * c - a * f) / (b * d - a * e);
+
+	cout << x << " " << y;
+	return 0;
+}
