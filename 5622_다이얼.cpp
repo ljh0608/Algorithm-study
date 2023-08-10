03422fcd3a7602aba772e36eef15fa6b952b4860
ljh0608 <m98m@catholic.ac.kr>
2023-08-10 13:44:42 +0900

    5622_다이얼

---
+++ 5622_다이얼.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+int N, M;
+int times[50] = { 3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10 };
+
+string phone;
+int result = 0;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> phone;
+	for (int i = 0; i<phone.length(); i++) {
+		result += times[phone[i] - 'A'];
+	}
+
+	cout << result;
+
+
+	return 0;
+}
+
