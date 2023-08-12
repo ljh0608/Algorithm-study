6e023954bb596f80b867304ce9ae9ecb88d2b016
ljh0608 <m98m@catholic.ac.kr>
2023-08-12 23:57:29 +0900

    27866_

---
+++ 27866_문자와문자열.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+using namespace std;
+
+string str;
+int num;
+int result = 0;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> str;
+	cin >> num;
+
+	cout << str[num-1];
+
+
+	return 0;
+}
+
