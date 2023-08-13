c61e9bfaf6f6e0540e2ea7bf12f56f44d46936ca
ljh0608 <m98m@catholic.ac.kr>
2023-08-13 23:29:54 +0900

    2444_별찍기-7

---
+++ 2444_별찍기-7.cpp
@@ -0,0 +1,45 @@
+﻿
+
+
+#define _CRT_SECURE_NO_WARNINGS 
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main() {
+		ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	for (int j = 1; j <= n; j++) {
+		for (int k = j; k < n; k++) {
+			cout << " ";
+		}
+		for (int i = 0; i < 2 * j - 1; i++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+	for (int j = n; j > 1; j--) {
+		for (int k = j; k <= n; k++) {
+			cout << " ";
+		}
+		for (int i = 2; i < 2 * j - 1; i++) {
+			cout << "*";
+		}
+		cout << "\n";
+	}
+
+
+
+
+
+	return 0;
+}
