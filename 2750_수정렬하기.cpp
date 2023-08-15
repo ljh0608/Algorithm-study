344a1949821714a99a527f5dc9ce259be2a6a47c
ljh0608 <m98m@catholic.ac.kr>
2023-08-15 23:58:35 +0900

    수정렬하기

---
+++ 2750_수정렬하기.cpp
@@ -0,0 +1,30 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS 
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+
+int N;
+vector<int>v;
+int main() {
+
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end());
+
+	
+	for (int i = 0; i < N; i++) {
+		cout << v[i]<<"\n";
+	}
+
+	return 0;
+}
+
