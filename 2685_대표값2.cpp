27c15bf6bace7bb53664e5ff8d59e00f43cfc75c
ljh0608 <m98m@catholic.ac.kr>
2023-08-16 16:11:14 +0900

    2685_대표값2

---
+++ 2685_대표값2.cpp
@@ -0,0 +1,39 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS 
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+
+vector<pair<int,int>> p;
+int N;
+int sum;
+
+int main() {
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		int x, y;
+		cin >> x;
+		cin >> y;
+
+		cout << "x: " << x << "y: " << y;
+		p.push_back(make_pair(x, y));
+	}
+	sort(p.begin(), p.end());
+
+	cout << "---------------\n";
+	for (int i = 0; i < N; i++) {
+		cout<< p[i];
+	}
+
+	//for (int i = 0; i < N; i++) {
+	//	cout << p[i].first << " "<< p[i].second;
+	//}
+
+	return 0;
+}
+
