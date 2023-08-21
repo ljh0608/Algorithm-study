20415c630db82c2b7be100f8f1bbcfcce0c53a45
ljh0608 <m98m@catholic.ac.kr>
2023-08-21 14:05:25 +0900

    11651_좌표정렬하기2

---
+++ 11651_좌표정렬하기2.cpp
@@ -0,0 +1,37 @@
+﻿
+#define _CRT_SECURE_NO_WARNINGS 
+#include<iostream>
+#include<algorithm>
+#include<vector>
+#include<string>
+#include<map>
+using namespace std;
+
+vector<int> v(0);
+
+int N,M;
+int sum;
+
+int main() {
+
+	cin >> N;
+	
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+		temp += 10000000;
+		v[i]++;
+	}
+	
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		int t;
+		cin >> t;
+		cout << v[t + 10000000] <<" ";
+	}
+
+
+	return 0;
+}
+
