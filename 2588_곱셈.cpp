f7c613167b004edc7850bed2aa74c5076742c376
ljh0608 <m98m@catholic.ac.kr>
2023-08-27 06:22:16 +0900

    2588_곱셈

---
+++ 2588_곱셈.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+vector<int> v;
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+	int sum = 0;
+	int c = 1;
+	while (1) {
+		
+		cout << a * (b % 10) << "\n";
+		sum += a * (b % 10)*c;
+		
+		c *= 10;
+		b /= 10;
+		if (!b) {
+			break;
+		}
+
+	}
+
+	cout << sum;
+
+	return 0;
+}
+
+
+
