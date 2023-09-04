cf8f8b95c31f8cf8b2db44bcd7c792584fd39e54
ljh0608 <m98m@catholic.ac.kr>
2023-09-04 20:42:26 +0900

    10718_welovekriii

---
+++ 10718_welovekriii.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+
+using namespace std;
+int N;
+
+
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    cout <<"강한친구 대한육군\n";
+
+    cout <<"강한친구 대한육군";
+
+    return 0;
+}
