67d6c3949595e363f81e45ad8309c7eb60f64916
ljh0608 <m98m@catholic.ac.kr>
2023-09-13 22:32:00 +0900

    1464_1로 만들기

--- 1463_1로만들기.cpp
+++ 1463_1로만들기.cpp
@@ -3,42 +3,41 @@
 #include <vector>
 
 using namespace std;
-int n,m;
+int n, m;
 int dp[1000001];
 
-void make_one(){
+void make_one()
+{
 
-    dp[1]=0;
-    dp[2]=1;
-    dp[3]=1;
-    for(int i=4;i<=n;i++){
-           dp[i]=dp[i-1]+1;
-           if(i%2==0){
-                dp[i]=min(dp[i],dp[i/2]+1);    
-  
-           }
-     
-            if(i%3==0){
-                    dp[i]=min(dp[i/3]+1,dp[i]);
+    dp[1] = 0;
+    dp[2] = 1;
+    dp[3] = 1;
+    for (int i = 4; i <= n; i++)
+    {
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
 
-            }
-         
- 
+        if (i % 3 == 0)
+        {   
+            dp[i] = min(dp[i / 3] + 1, dp[i]);
+        }
     }
 }
 
-
-int main(){
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
- 
-    cin>>n;
 
-    fill(dp,dp+100000,1e9);
+    cin >> n;
+    fill(dp, dp + 100000, 1e9);
     make_one();
-    
-    cout<<dp[n];
+
+    cout << dp[n];
     
     return 0;
 }
